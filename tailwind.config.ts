7213bfa1a3099cdd5c53c37d37b7064e9ed4f045
Petar Gjorgjioski <pgpetar3@gmail.com>
2024-10-19 11:18:59 +0200

    fix tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,7 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
-  content: ["./index.html", "./src/**/*.{ts,tsx, js, jsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
 };
-
-export default config;
