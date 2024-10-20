ecadd20f866d6baf181a370fc8b27a7d9344b9e8
JohniO34 <jovanmitres@gmail.com>
2024-10-20 18:25:52 +0200

    Courses dashboard ne dovrsena

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './assets/pages/HomePage/HomePage';
 import UserDashboard from './assets/pages/UserDashboard/UserDashboard';
+import CoursesDashboard from './assets/pages/CoursesDashboard/CoursesDashboard';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/user-dashboard/main-panel" element={<UserDashboard />} />
+        <Route path="/courses-dashboard/main-panel" element={<CoursesDashboard />} />
       </Routes>
     </Router>
   );
